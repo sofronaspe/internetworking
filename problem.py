9b0b6a4aff7c1d8eb619e7fceb18a07c2dc6b456
Peter <sofronas96@gmail.com>
2016-02-02 17:12:45 -0500

    Finished table and working on subnet mask

--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
 import random, math
+ip = str(random.randint(0,223) + '.' + random.randint(0,255) + '.' + random.randint(0,255) + '.' + random.randint(0,255))
 ip = ".".join(map(str, (random.randint(0, 255) for x in range(4))))
 # networksNeeded = random.randint(1,16)
 networksNeeded = 10
@@ -21,10 +22,6 @@ def findClass(x):
 
 def printTable():
     table = [[] for i in range(subNetCount)]
-    subnetSize = int(math.log(subNetCount,2))
-    hostSize = 8-subnetSize
-    NAHost = '0' * hostSize
-    BAHost = '1' * hostSize
 
     for i in range(subNetCount):
         s = i
@@ -34,43 +31,50 @@ def printTable():
         r = '.' + str(ndec + 1) + ' - .' + str(bdec + 1)
         table[i] = [s, sub.zfill(subnetSize),NAHost,ndec,sub.zfill(subnetSize),BAHost,bdec,r]
     table.insert(0,['Subnet',"N subnet", "N host", "=", "B subnet", "B host", "=", "Range"])
-    #print 'Subnet',"NA subnet", "NA host", " = ", "BA subnet", "BA host", " = ", "Range"
 
     col_width = [max(len(str(x)) for x in col) for col in zip(*table)]
     for line in table:
         print "| " + " | ".join("{:{}}".format(x, col_width[i])
                                 for i, x in enumerate(line)) + " |"
-    # for i in range(len(table)):
-    #     for j in table[i]:
-    #         print repr(j).rjust(4),
-    #     print
-        # repr(i[7]).rjust(11)
-
 
 print 'The IP address is ' + ip + "\nYou need " + str(networksNeeded) + " subnetworks"
 
 ipClass = findClass(int(n[0]))
 
-# classGuess = raw_input('\nWhat is the class of the IP?  ')
-# classGuess.lower()
-# if classGuess == ipClass:
-#     print 'Correct'
-# else:
-#     print 'Incorrect, the class is ' + ipClass
+classGuess = raw_input('\nWhat is the class of the IP?  ')
+classGuess.lower()
+if classGuess == ipClass:
+    print 'Correct'
+else:
+    print 'Incorrect, the class is ' + ipClass
 
 subNetCount = 2 ** len("{0:b}".format(networksNeeded))
-# subnetGuess = raw_input('\nHow many subnetworks do you need?  ')
-#
-# if int(subnetGuess) == subNetCount:
-#     print 'Correct'
-# else:
-#     print 'Incorrect, ' + subNetCount + ' subnetworks are needed'
+subnetGuess = raw_input('\nHow many subnetworks will be created?  ')
 
-usableHosts = math.log(subNetCount,2) - 2
-# hostGuess = raw_input('\nHow many usable hosts can are there?  ')
-# if int(hostGuess) == usableHosts:
-#     print 'Correct'
-# else:
-#     print 'Incorrect, there are ' + usableHosts + " usable hosts"
+if int(subnetGuess) == subNetCount:
+    print 'Correct'
+else:
+    print 'Incorrect, ' + subNetCount + ' subnetworks are needed'
 
+subnetSize = int(math.log(subNetCount,2))
+hostSize = 8-subnetSize
+NAHost = '0' * hostSize
+BAHost = '1' * hostSize
+hostCount = 2 ** hostSize - 2
+hostGuess = raw_input('\nHow many usable hosts are there?  ')
+if int(hostGuess) == hostCount:
+    print 'Correct'
+else:
+    print 'Incorrect, there are ' + str(hostCount) + ' usable hosts'
+raw_input('Press Enter to view table')
 printTable()
+maskGuess = raw_input('What will the subnet mask be (whole IP)? ')
+mask = ''
+if ipClass == 'a':
+    mask = '255.255.255.'+ str(2 ** subnetSize - 1)
+elif ipClass == 'b':
+    mask = '255.255.'+ str(2 ** subnetSize - 1) + '.0'
+elif ipClass == 'c':
+    mask = '255.'+ str(2 ** subnetSize - 1) + '0.0'
+else:
+    print 'Something went wrong'
